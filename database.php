5ba336191e3a843d30627065f8deedc6f6474418
mohammedelmaddahi <54798680+mohammedelmaddahi@users.noreply.github.com>
2021-06-16 23:31:59 +0200

    Update database.php

--- database.php
+++ database.php
@@ -1,4 +1,4 @@
-<<?php 
+<?php 
 
 Class database{
     private $host;
@@ -12,9 +12,22 @@ Class database{
             $this->username = 'root';
             $this->password = '';
             $this->database = 'jongerenkansrijker';
-
             
+             $dsn = "mysql:host=$this->host;dbname=$this->database;charset=utf8mb4";
+            
+            $options = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+            try {
+             $this->dbh = new PDO($dsn, $this->username, $this->password, $options);
+        } catch (\PDOException $e) {
+             throw new \PDOException($e->getMessage(), (int)$e->getCode());
         }
+            
+     }
 
 
 
